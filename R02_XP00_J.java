4f7873f2d8df383e5a4651b9092e077109af2118
Elena <berishaelena@gmail.com>
2020-10-26 23:55:25 -0400

    add compliant code

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,10 +1,13 @@
 /*Rule 02. Expressions (EXP)
 XP00-J. Do not ignore values returned by methods
 */
+
 public void deleteFile(){
 
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  someFile.delete();
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
 
 }
--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -1,7 +1,8 @@
 /* Rule 08. Visibility and Atomicity (VNA)
 VNA00-J. Ensure visibility when accessing shared primitive variables */
+
 final class ControlledStop implements Runnable {
-  private boolean done = false;
+  private volatile boolean done = false;
 
   @Override public void run() {
     while (!done) {
